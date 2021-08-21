499f6529d65cf590edee51fb5a17a93a173f1603
jari <mr.jari.kuusisto@gmail.com>
2021-08-22 00:00:01 +0300

    Added example CLI app from picocli.info docs

--- src/main/java/org/example/main/Application.java
+++ src/main/java/org/example/main/Application.java
@@ -1,8 +1,11 @@
 package org.example.main;
 
+import picocli.CommandLine;
+
 public class Application {
 
-    public static void main(String[] args) {
-
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Checksum()).execute(args);
+        System.exit(exitCode);
     }
 }
